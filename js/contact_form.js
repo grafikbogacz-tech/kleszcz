a6ef0caea4974d733cbe12145b52528acce360d7
grafikbogacz-tech <grafik.bogacz@gmail.com>
2026-04-04 00:38:27 +0200

    Projekt

---
+++ js/contact_form.js
@@ -0,0 +1,50 @@
+$(function() {
+
+	"use strict";
+
+	$('.contact-form').on("submit", function(){
+		var $this = $(this);
+						   
+		$('.invalid').removeClass('invalid');						   
+		var msg = 'Wypełnij wymagane pola:',
+			successMessage = "Dziękujemy! Odpiszemy na Twoją wiadomość tak szybko jak to możliwe.",
+			error = 0,
+			pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+
+		if ($.trim($('.contact-form input[name="name"]').val()) === '') {error = 1; $this.find('input[name="name"]').parent().addClass('invalid'); msg = msg +  '\n - Imię i nazwisko';}
+        if (!pattern.test($.trim($('.contact-form input[name="email"]').val()))) {error = 1; $this.find('input[name="email"]').parent().addClass('invalid'); msg = msg +  '\n - E-mail';}
+		if ($.trim($('.contact-form textarea[name="message"]').val()) === '') {error = 1; $this.find('textarea[name="message"]').parent().addClass('invalid'); msg = msg +  '\n - Wiadomość';}
+
+        if (error){
+        	updateTextPopup('BŁĄD', msg);
+        }else{
+            var url = 'send_mail.php',
+            	name = $.trim($this.find('input[name="name"]').val()),
+            	email = $.trim($this.find('input[name="email"]').val()),
+            	subject = ($this.find('input[name="subject"]').length)?$.trim($this.find('input[name="subject"]').val()):'',
+            	message = $.trim($this.find('textarea[name="message"]').val());
+
+            $.post(url,{'name':name,'email':email,'subject':subject,'message':message},function(data){
+	        	updateTextPopup('DZIĘKUJEMY!', successMessage);
+	        	$this.append('<input type="reset" class="reset-button"/>');
+	        	$('.reset-button').click().remove();
+	        	$this.find('.focus').removeClass('focus');
+			}).fail(function(){
+				updateTextPopup('BŁĄD', 'Nie udało się wysłać wiadomości. Spróbuj ponownie lub zadzwoń.');
+			});
+        }
+	  	return false;
+	});
+
+	$(document).on('keyup', '.input-wrapper .input', function(){
+		$(this).parent().removeClass('invalid');
+	});
+
+	function updateTextPopup(title, text){
+		$('.text-popup .text-popup-title').text(title);
+		$('.text-popup .text-popup-message').text(text);
+		$('.text-popup').addClass('active');
+	}
+
+});
